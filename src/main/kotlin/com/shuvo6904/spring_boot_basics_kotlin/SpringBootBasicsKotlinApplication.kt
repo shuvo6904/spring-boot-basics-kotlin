442d9ba4d7c45fe518ea4c8d67480afb95bc0d60
shuvo <shuvo6904@gmail.com>
2025-07-30 20:39:33 +0600

    Initial commit

---
+++ src/main/kotlin/com/shuvo6904/spring_boot_basics_kotlin/SpringBootBasicsKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.shuvo6904.spring_boot_basics_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootBasicsKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootBasicsKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/shuvo6904/spring_boot_basics_kotlin/SpringBootBasicsKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.shuvo6904.spring_boot_basics_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootBasicsKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
